583ee33df6c89c9d97f8a28ffd5603e219aa90cb
pqs <2945802136@qq.com>
2019-05-12 15:35:00 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,4 @@
-echo "# pubuliu" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git remote add origin https://github.com/pengqiangsheng/pubuliu.git
-git push -u origin master
+git status
+git add .
+git commit -m "update"
+git push
